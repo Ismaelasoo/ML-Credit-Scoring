7961e825cf257d75032281770deda5a794bfdc4e
Ismaelasoo <ismaelantoniolasotrillo@gmail.com>
2024-12-24 10:25:54 +0100

    24/12/24

--- modulos/eda.py
+++ modulos/eda.py
@@ -4,3 +4,10 @@ import pandas as pd
 def head_tail ( df, n ):
     separator = pd.DataFrame([["..."] * df.shape[1]], columns=df.columns)
     return pd.concat([df.head(n), separator, df.tail(n)], ignore_index = True)
+
+def describe_columna(df, col):
+    print(f'Columna: {col}  -  Tipo de datos: {df[col].dtype}')
+    print(f'Número de valores nulos: {df[col].isnull().sum()}  -  Número de valores distintos: {df[col].nunique()}')
+    print('Valores más frecuentes:')
+    for i, v in df[col].value_counts().iloc[:10].items() :
+        print(i, '\t', v)
