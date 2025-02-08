e933fa51de9de892aa35a7a9b7c6ef31054cffea
Ismaelasoo <ismaelantoniolasotrillo@gmail.com>
2025-02-08 11:26:54 +0100

    Finish_EDA

--- modulos/data_visualization.py
+++ modulos/data_visualization.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from IPython.display import Markdown, display
